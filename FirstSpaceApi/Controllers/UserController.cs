0f028e862ddeebc084a390afdd2dd2a7cdcc1e90
Shubham Yadav <sy7833446@gmail.com>
2024-02-13 01:13:47 +0530

    Implemented ServiceProvider, UnitOfWork, DTO

--- FirstSpaceApi/Controllers/UserController.cs
+++ FirstSpaceApi/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using FirstSpaceApi.Shared.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using FSServiceProvider = FirstSpaceApi.Services.IService;
 
 namespace FirstSpaceApi.Controllers
 {
@@ -9,72 +10,76 @@ namespace FirstSpaceApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IUserRepository _userRepository;
+        private FSServiceProvider.IServiceProvider _serviceProvider;
 
-        public UserController(IUserRepository userRepository)
+        public UserController(FSServiceProvider.IServiceProvider serviceProvider)
         {
-            _userRepository = userRepository;
+            _serviceProvider = serviceProvider;
         }
 
         [HttpGet]
         public IActionResult GetAllUserAsync()
         {
-            var allUsers =  _userRepository.GetUserDetails();
-            if (allUsers == null)
+            try
             {
-                return NotFound(); // No users found
+                var users = _serviceProvider.UserService.GetAllUser(trackChanges: false);
+                return Ok(users);
             }
+            catch
+            {
+                return StatusCode(500, "Internal server error");
 
-            return Ok(allUsers);
+            }
         }
 
-        [HttpPost("CreateUser")]
-        public IActionResult CreateUser([FromBody] User user)
-        {
-            var addedUser = _userRepository.AddUser(user);
-            if (addedUser == null)
-            {
-                return NotFound(); // No users found
-            }
+        
+        //[HttpPost("CreateUser")]
+        //public IActionResult CreateUser([FromBody] User user)
+        //{
+        //    var addedUser = _userRepository.AddUser(user);
+        //    if (addedUser == null)
+        //    {
+        //        return NotFound(); // No users found
+        //    }
 
-            return Ok(addedUser);
-        }
+        //    return Ok(addedUser);
+        //}
 
-        [HttpGet("{userId}")]
-        public IActionResult GetUserByID(string userId)
-        {
-            var addedUser = _userRepository.GetUserDetailById(userId);
-            if (addedUser == null)
-            {
-                return NotFound(); // No users found
-            }
+        //[HttpGet("{userId}")]
+        //public IActionResult GetUserByID(string userId)
+        //{
+        //    var addedUser = _userRepository.GetUserDetailById(userId);
+        //    if (addedUser == null)
+        //    {
+        //        return NotFound(); // No users found
+        //    }
 
-            return Ok(addedUser);
-        }
+        //    return Ok(addedUser);
+        //}
 
-        [HttpPut("UpdateUser")]
-        public IActionResult UpdateUser([FromBody] User user)
-        {
-            var updatedUser = _userRepository.UpdateUser(user);
-            if (updatedUser == null)
-            {
-                return NotFound(); // No users found
-            }
+        //[HttpPut("UpdateUser")]
+        //public IActionResult UpdateUser([FromBody] User user)
+        //{
+        //    var updatedUser = _userRepository.UpdateUser(user);
+        //    if (updatedUser == null)
+        //    {
+        //        return NotFound(); // No users found
+        //    }
 
-            return Ok(updatedUser);
-        }
+        //    return Ok(updatedUser);
+        //}
 
-        [HttpDelete("Remove/{userId}")]
-        public IActionResult RemoveUserById(string userId)
-        {
-            var deletedUser = _userRepository.DeleteUser(userId);
-            if (deletedUser == null)
-            {
-                return NotFound(); // No users found
-            }
+        //[HttpDelete("Remove/{userId}")]
+        //public IActionResult RemoveUserById(string userId)
+        //{
+        //    var deletedUser = _userRepository.DeleteUser(userId);
+        //    if (deletedUser == null)
+        //    {
+        //        return NotFound(); // No users found
+        //    }
 
-            return Ok(deletedUser);
-        }
+        //    return Ok(deletedUser);
+        //}
 
 
     }
