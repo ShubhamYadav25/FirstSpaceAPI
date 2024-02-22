498ef586a1c26d3376fa2e4a596f32bff81ec7d8
Shubham Yadav <sy7833446@gmail.com>
2024-02-23 00:01:25 +0530

    Will implement later

--- FirstSpaceApi/Filters/ValidationFilterAttribute.cs
+++ FirstSpaceApi/Filters/ValidationFilterAttribute.cs
@@ -1,28 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace FirstSpaceApi.Filters
-{
-    public class ValidationFilterAttribute
-    {
-        public ValidationFilterAttribute() { }
+//namespace FirstSpaceApi.Filters
+//{
+//    public class ValidationFilterAttribute
+//    {
+//        public ValidationFilterAttribute() { }
 
-        // Before action
-        public void OnActionExecuting(ActionExecutingContext context) {
-            var action = context.RouteData.Values["action"];
-            var controller = context.RouteData.Values["controller"];
+//        // Before action
+//        public void OnActionExecuting(ActionExecutingContext context) {
+//            var action = context.RouteData.Values["action"];
+//            var controller = context.RouteData.Values["controller"];
 
-            var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
-            if (param is null)
-            {
-                context.Result = new BadRequestObjectResult($"Object is null. Controller: { controller }, action: { action}");
-            return;
-            }
-            if (!context.ModelState.IsValid)
-                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
-        }
+//            var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
+//            if (param is null)
+//            {
+//                context.Result = new BadRequestObjectResult($"Object is null. Controller: { controller }, action: { action}");
+//            return;
+//            }
+//            if (!context.ModelState.IsValid)
+//                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+//        }
 
-        public void OnActionExecuted(ActionExecutedContext context) { }
+//        public void OnActionExecuted(ActionExecutedContext context) { }
 
-    }
-}
+//    }
+//}
